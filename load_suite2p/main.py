55dbc66ab46769141fbd7264ff3fca2ef6ee8113
lauraporta <29216006+lauraporta@users.noreply.github.com>
2022-11-15 13:40:01 +0000

    Add logging draft

--- load_suite2p/main.py
+++ load_suite2p/main.py
@@ -1,4 +1,14 @@
+import logging
+
 from read_config import read
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="app.log",
+    filemode="w",
+    format="%(asctime)s|%(process)d|%(name)s|%(levelname)s|%(message)s",
+)
+logger = logging.getLogger(__name__)
 
 
 def main():
