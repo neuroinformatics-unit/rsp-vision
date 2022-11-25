2d91c69c02b0d1203b778137495ea04e97f87a81
lauraporta <ucqflpo@ucl.ac.uk>
2022-11-25 14:48:35 +0000

    Fix error missing positional argument

--- load_suite2p/formatted_data.py
+++ load_suite2p/formatted_data.py
@@ -4,12 +4,9 @@ from .folder_naming_specs import FolderNamingSpecs
 class FormattedData:
     """Class to load the formatted data from suite2p and registers2p."""
 
-    def __init__(
-        self,
-        file_name: str,
-    ):
+    def __init__(self, file_name: str, config: dict):
         self.file_name = file_name
-        self.file_specs = self.get_FileNamingSpecs()
+        self.file_specs = self.get_FileNamingSpecs(config)
 
-    def get_FileNamingSpecs(self) -> FolderNamingSpecs:
-        return FolderNamingSpecs(self.file_name)
+    def get_FileNamingSpecs(self, config) -> FolderNamingSpecs:
+        return FolderNamingSpecs(self.file_name, config)
