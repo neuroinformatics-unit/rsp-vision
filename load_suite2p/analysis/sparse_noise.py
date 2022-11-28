6c49596c03bab5fb4393ac030ebece1904da272c
lauraporta <29216006+lauraporta@users.noreply.github.com>
2022-11-28 18:45:43 +0000

    Add scaffold of business logic around main three scripts of the original repo

---
+++ load_suite2p/analysis/sparse_noise.py
@@ -0,0 +1,10 @@
+from ..load.formatted_data import PhotonData, PhotonOptions
+
+
+class SparseNoise:
+    def __init__(self, data: PhotonData, options: PhotonOptions):
+        self.data = data
+        self.options = options
+
+    def responsiveness(self):
+        raise NotImplementedError("This method is not implemented yet")
