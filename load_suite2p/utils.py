83e68fb9840915d8f08af9b6c656c65228729edc
lauraporta <29216006+lauraporta@users.noreply.github.com>
2022-12-12 13:28:11 +0000

    Remove check connection method

--- load_suite2p/utils.py
+++ load_suite2p/utils.py
@@ -1,12 +1,7 @@
-import logging
 import sys
 
 import rich
 from fancylog import fancylog
-from vpn_server_connections.connections import (
-    can_ping_swc_server,
-    is_winstor_mounted,
-)
 
 
 def get_module_for_logging() -> object:
@@ -54,39 +49,3 @@ def start_logging():
     fancylog.start_logging(
         output_dir="./", package=module, filename="load_suite2p", verbose=False
     )
-
-
-def check_connection(config: dict):
-    """Check if the connection to the server is established and if Winstor
-    is mounted.
-
-    Parameters
-    ----------
-    config : dict
-        Configuration file to load the data
-
-    Raises
-    ------
-    RuntimeError
-        If the connection to the VPN is not established
-    RuntimeError
-        If Winstor is not mounted
-    RuntimeError
-        If the configuration file is not correct
-    """
-    if not (
-        ("server" in config)
-        and ("paths" in config)
-        and ("winstor" in config["paths"])
-    ):
-        raise RuntimeError(
-            "The configuration file is not complete."
-            + "Please check the documentation."
-        )
-
-    if not can_ping_swc_server(config["server"]):
-        logging.debug("Please connect to the VPN.")
-        raise RuntimeError("Please connect to the VPN.")
-    if not is_winstor_mounted(config["paths"]["winstor"]):
-        logging.debug("Please mount Winstor.")
-        raise RuntimeError("Please mount Winstor.")
