7d7c9dfaca9def1889e939c6d7052895d8388d04
lauraporta <29216006+lauraporta@users.noreply.github.com>
2023-01-06 15:13:55 +0000

    Change to h5py

--- load_suite2p/load/load_data.py
+++ load_suite2p/load/load_data.py
@@ -2,7 +2,7 @@ import logging
 from pathlib import Path
 from typing import Tuple
 
-import mat73
+import h5py
 
 from ..objects.data_raw import DataRaw
 from ..objects.enums import AnalysisType, DataType
@@ -65,7 +65,7 @@ def load(specs: Specifications) -> DataRaw:
             ]
             if len(allen_data_files) == 1:
                 data_raw = DataRaw(
-                    mat73.loadmat(allen_data_files[0].path), is_allen=True
+                    h5py.File(allen_data_files[0].path, 'r'), is_allen=True
                 )
                 logging.info("Allen data loaded")
                 return data_raw
