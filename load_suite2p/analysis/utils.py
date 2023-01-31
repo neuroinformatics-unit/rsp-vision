d6687a297a88a8cb975ef2223b1876dfd5304deb
lauraporta <29216006+lauraporta@users.noreply.github.com>
2023-01-31 15:58:08 +0000

    Refactor and add documentation

--- load_suite2p/analysis/utils.py
+++ load_suite2p/analysis/utils.py
@@ -1,2 +1,16 @@
+from ..objects.enums import PhotonType
+
+
 def ascii_array_to_string(array):
     return "".join([chr(int(i)) for i in array])
+
+
+def get_fps(photon_type):
+    if photon_type == PhotonType.TWO_PHOTON:
+        return 30
+    elif photon_type == PhotonType.THREE_PHOTON:
+        return 15
+    else:
+        raise NotImplementedError(
+            "Unknown number of frames per second for this photon type"
+        )
--- load_suite2p/objects/photon_data.py
+++ load_suite2p/objects/photon_data.py
@@ -6,22 +6,6 @@ from .data_raw import DataRaw
 class PhotonData:
     def __init__(self, data_raw: DataRaw):
 
-        # initialize valiable from data_raw that are straight forward
-        self.is_cell = data_raw.is_cell
-        self.day_roi = data_raw.day["roi"]
-        self.day_roi_label = data_raw.day["roi_label"]
-
-        # hard coded values
-        self.padding = [25, 50]
-        self.drift_order = 2
-        self.dim_name = [
-            "frame",
-            "trial",
-            "orientation",
-            "spatial_frequency",
-            "temporal_frequency",
-            "roi",
-        ]
         self.screen_size = data_raw.stim[0]["screen_size"]
 
         self.mapper = Remapping(data_raw, PhotonType.TWO_PHOTON)
