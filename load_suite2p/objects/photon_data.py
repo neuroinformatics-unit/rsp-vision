cea8dd852c91331c190c27a8d0cc4ee00c5438be
lauraporta <29216006+lauraporta@users.noreply.github.com>
2023-01-31 15:58:52 +0000

    Move attribute

--- load_suite2p/objects/photon_data.py
+++ load_suite2p/objects/photon_data.py
@@ -6,7 +6,5 @@ from .data_raw import DataRaw
 class PhotonData:
     def __init__(self, data_raw: DataRaw):
 
-        self.screen_size = data_raw.stim[0]["screen_size"]
-
         self.mapper = Remapping(data_raw, PhotonType.TWO_PHOTON)
         self.signal = self.mapper.make_signal_dataframe(data_raw)
