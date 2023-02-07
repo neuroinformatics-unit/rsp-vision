0b95d91941869cd3f9d3ea3effb8d1cf934f0182
lauraporta <29216006+lauraporta@users.noreply.github.com>
2023-02-06 16:25:52 -0800

    Add setup instruction to help reviewing the code

---
+++ setup_for_demo.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+import yaml
+
+if __name__ == "__main__":
+    # create .env file and add the path to config
+    f = open("test.env", "x")
+
+    with open("test.env", "w") as f:
+        f.write('CONFIG_PATH="config-test/config.yml"')
+
+    # create config folder
+    Path("load_suite2p/config-test").mkdir(parents=True, exist_ok=True)
+
+    #  create config file and store it in config folder
+    f = open("load_suite2p/config-test/config.yml", "x")
+
+    with open("load_suite2p/config-test/config.yml", "w") as f:
+        content = {
+            "parser": "Parser2pRSP",
+            "paths": {
+                "imaging": "/path/to/",
+                "allen-dff": "/path/to/allen_dff",
+                "serial2p": "/path/to/serial2p",
+                "stimulus-ai-schedule": "/path/to/stimulus_AI_schedule_files",
+            },
+            "use-allen-dff": "true",
+            "analysis-type": "sf_tf",
+        }
+
+        yaml.dump(content, f)
