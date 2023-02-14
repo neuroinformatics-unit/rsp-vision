aff65fc31afbe1eacab07eaf238d0c2381171329
lauraporta <ucqflpo@ucl.ac.uk>
2023-02-14 10:36:46 -0800

    Edit setup for demo

--- setup_for_demo.py
+++ setup_for_demo.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
         f.write('CONFIG_PATH="config/config.yml"')
 
     # create config folder
-    Path("load_suite2p/config-test").mkdir(parents=True, exist_ok=True)
+    Path("load_suite2p/config").mkdir(parents=True, exist_ok=True)
 
     #  create config file and store it in config folder
-    f = open("load_suite2p/config-test/config.yml", "x")
+    f = open("load_suite2p/config/config.yml", "x")
 
-    with open("load_suite2p/config-test/config.yml", "w") as f:
+    with open("load_suite2p/config/config.yml", "w") as f:
         content = {
             "parser": "Parser2pRSP",
             "paths": {
