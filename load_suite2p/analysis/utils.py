2e14e87cf7a9b3607870e47354383867e07b1654
lauraporta <29216006+lauraporta@users.noreply.github.com>
2023-02-15 12:01:46 -0800

    Add fps to config file

--- load_suite2p/analysis/utils.py
+++ load_suite2p/analysis/utils.py
@@ -5,11 +5,11 @@ def ascii_array_to_string(array):
     return "".join([chr(int(i)) for i in array])
 
 
-def get_fps(photon_type):
+def get_fps(photon_type, config: dict):
     if photon_type == PhotonType.TWO_PHOTON:
-        return 30
+        return config["fps_two_photon"]
     elif photon_type == PhotonType.THREE_PHOTON:
-        return 15
+        return config["fps_three_photon"]
     else:
         raise NotImplementedError(
             "Unknown number of frames per second for this photon type"
