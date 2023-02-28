7294f3da3996c107b4c38506349b8fdb3a3970f6
lauraporta <29216006+lauraporta@users.noreply.github.com>
2023-02-28 17:24:11 +0000

    Add magintude calculations and refactoring

--- rsp_vision/analysis/spatial_freq_temporal_freq.py
+++ rsp_vision/analysis/spatial_freq_temporal_freq.py
@@ -18,12 +18,7 @@ class SF_TF:
         self.padding_start = int(data.config["padding"][0])
         self.padding_end = int(data.config["padding"][1])
 
-        self.calculate_mean_response_and_baseline()
-
-    def calculate_mean_response_and_baseline(
-        self,
-    ):
-        stimulus_idxs = self.data.signal[
+        self.stimulus_idxs = self.data.signal[
             self.data.signal["stimulus_onset"]
         ].index
 
@@ -31,6 +26,7 @@ class SF_TF:
         self.adapted_signal["mean_response"] = np.nan
         self.adapted_signal["mean_baseline"] = np.nan
 
+    def get_response_and_baseline_windows(self):
         baseline_start = 0
         if self.data.n_triggers_per_stimulus == 3:
             response_start = 2
@@ -39,17 +35,15 @@ class SF_TF:
         if self.data.n_triggers_per_stimulus == 2:
             response_start = 1
 
-        logging.info("Start to edit the signal dataframe...")
-
         # Identify the rows in the window of each stimulus onset
         window_start_response = (
-            stimulus_idxs
+            self.stimulus_idxs
             + (self.data.n_frames_per_trigger * response_start)
             + (self.fps * 0.5)  # ignore first 0.5s
             - 1
         )
         window_end_response = (
-            stimulus_idxs
+            self.stimulus_idxs
             + (self.data.n_frames_per_trigger * (response_start + 1))
             - 1
         )
@@ -63,13 +57,13 @@ class SF_TF:
         )
 
         window_start_baseline = (
-            stimulus_idxs
+            self.stimulus_idxs
             + (self.data.n_frames_per_trigger * baseline_start)
             + (self.fps * 1.5)  # ignore first 1.5s
             - 1
         )
         window_end_baseline = (
-            stimulus_idxs
+            self.stimulus_idxs
             + (self.data.n_frames_per_trigger * (baseline_start + 1))
             - 1
         )
@@ -82,30 +76,40 @@ class SF_TF:
             ]
         )
 
+        return window_mask_response, window_mask_baseline
+
+    def calculate_mean_response_and_baseline(
+        self,
+    ):
+        logging.info("Start to edit the signal dataframe...")
+
+        (
+            self.window_mask_response,
+            self.window_mask_baseline,
+        ) = self.get_response_and_baseline_windows()
+
         mean_response_signal = [
             np.mean(
                 self.adapted_signal.iloc[
-                    window_mask_response[i]
-                ].signal.values,
-                axis=0,
+                    self.window_mask_response[i]
+                ].signal.values
             )
-            for i in range(len(window_mask_response))
+            for i in range(len(self.window_mask_response))
         ]
         mean_baseline_signal = [
             np.mean(
                 self.adapted_signal.iloc[
-                    window_mask_baseline[i]
-                ].signal.values,
-                axis=0,
+                    self.window_mask_baseline[i]
+                ].signal.values
             )
-            for i in range(len(window_mask_baseline))
+            for i in range(len(self.window_mask_baseline))
         ]
 
         self.adapted_signal.loc[
-            stimulus_idxs, "mean_response"
+            self.stimulus_idxs, "mean_response"
         ] = mean_response_signal
         self.adapted_signal.loc[
-            stimulus_idxs, "mean_baseline"
+            self.stimulus_idxs, "mean_baseline"
         ] = mean_baseline_signal
 
         self.adapted_signal["subtracted"] = (
@@ -128,6 +132,7 @@ class SF_TF:
                 "subtracted",
             ]
         ]
+        self.responses = self.responses.reset_index()
 
     def get_fit_parameters(self):
         # calls _fit_two_dimensional_elliptical_gaussian
@@ -152,6 +157,77 @@ class SF_TF:
         ) = self.are_responses_significant()
         logging.info(f"P-values for each roi:\n{self.p_values}")
 
+        self.magintude_over_medians = self.response_magnitude()
+
+    def response_magnitude(self):
+        # get specific windows for each sf/tf combo
+
+        magintude_over_medians = pd.DataFrame(
+            columns=[
+                "roi",
+                "sf",
+                "tf",
+                "response_mean",
+                "baseline_mean",
+                "baseline_std",
+                "magnitude",
+            ]
+        )
+
+        for roi in range(self.data.n_roi):
+            for i, sf_tf in enumerate(self.sf_tf_combinations):
+                sf_tf_idx = self.responses[
+                    (self.responses.sf == sf_tf[0])
+                    & (self.responses.tf == sf_tf[1])
+                    & (self.responses.roi_id == roi)
+                ].index
+                r_windows = self.window_mask_response[sf_tf_idx]
+                b_windows = self.window_mask_baseline[sf_tf_idx]
+
+                responses_dir_and_reps = np.zeros(
+                    (r_windows.shape[0], r_windows.shape[1])
+                )
+                baseline_dir_and_reps = np.zeros(
+                    (b_windows.shape[0], b_windows.shape[1])
+                )
+
+                for i, w in enumerate(r_windows):
+                    responses_dir_and_reps[i, :] = self.adapted_signal.iloc[
+                        w
+                    ].signal.values
+
+                for i, w in enumerate(b_windows):
+                    baseline_dir_and_reps[i, :] = self.adapted_signal.iloc[
+                        w
+                    ].signal.values
+
+                median_response = np.median(responses_dir_and_reps, axis=0)
+                median_baseline = np.median(baseline_dir_and_reps, axis=0)
+
+                m_r = np.mean(median_response)
+                m_b = np.mean(median_baseline)
+                std_b = np.std(median_baseline, ddof=1)
+                magnitude = (m_r - m_b) / std_b
+
+                df = pd.DataFrame(
+                    {
+                        "roi": roi,
+                        "sf": sf_tf[0],
+                        "tf": sf_tf[1],
+                        "response_mean": m_r,
+                        "baseline_mean": m_b,
+                        "baseline_std": std_b,
+                        "magnitude": magnitude,
+                    },
+                    index=[0],
+                )
+
+                magintude_over_medians = pd.concat(
+                    [magintude_over_medians, df], ignore_index=True
+                )
+
+        return magintude_over_medians
+
     def nonparam_anova_over_rois(self) -> dict:
         # Use Kruskal-Wallis H Test because it is nonparametric.
         # Compare if more than two independent
@@ -165,21 +241,21 @@ class SF_TF:
                 value_vars=["subtracted"],
             )
 
-            _sf = np.sort(self.data.uniques["sf"])
-            _tf = np.sort(self.data.uniques["tf"])
-            sf_tf_combinations = np.array(np.meshgrid(_sf, _tf)).T.reshape(
-                -1, 2
-            )
-            _dir = self.data.uniques["direction"]
+            self._sf = np.sort(self.data.uniques["sf"])
+            self._tf = np.sort(self.data.uniques["tf"])
+            self.sf_tf_combinations = np.array(
+                np.meshgrid(self._sf, self._tf)
+            ).T.reshape(-1, 2)
+            self._dir = self.data.uniques["direction"]
 
             samples = np.zeros(
                 (
-                    len(_dir) * self.data.n_triggers_per_stimulus,
-                    len(sf_tf_combinations),
+                    len(self._dir) * self.data.n_triggers_per_stimulus,
+                    len(self.sf_tf_combinations),
                 )
             )
 
-            for i, sf_tf in enumerate(sf_tf_combinations):
+            for i, sf_tf in enumerate(self.sf_tf_combinations):
                 samples[:, i] = melted[
                     (melted.sf == sf_tf[0]) & (melted.tf == sf_tf[1])
                 ].value
