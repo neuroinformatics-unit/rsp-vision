c6b41cff18efc13987a99650f955002378d93e6f
lauraporta <ucqflpo@ucl.ac.uk>
2023-03-06 13:16:37 +0100

    Change imports

--- rsp_vision/main.py
+++ rsp_vision/main.py
@@ -1,14 +1,14 @@
 from rich.prompt import Prompt
 
-from .analysis.spatial_freq_temporal_freq import FrequencyAnalysis
-from .load.load_data import load_data
-from .objects.enums import PhotonType
-from .objects.photon_data import PhotonData
-from .plots.plotter import Plotter
-from .utils import exception_handler, start_logging
+from analysis.spatial_freq_temporal_freq import FrequencyAnalysis
+from load.load_data import load_data
+from objects.enums import PhotonType
+from objects.photon_data import PhotonData
+from plots.plotter import Plotter
+from utils import exception_handler, start_logging
 
 
-@exception_handler
+
 def main():
     """Entry point of the program. CLI or GUI functionality is added here."""
     # pipeline draft
@@ -22,6 +22,21 @@ def main():
         Example: AK_1111739_hL_RSPd_monitor_front\n \
         📁"
     )
+
+    pipeline(folder_name)
+    
+    # Plots
+    # plotter = Plotter(analysis)
+
+    # plotter.murakami_plot()
+    # plotter.anova_window_plot()
+    # plotter.polar_grid()
+    # plotter.response_map()
+    # plotter.sftf_fit()
+    # plotter.traceplot()
+
+@exception_handler
+def pipeline(folder_name):
     photon_type = PhotonType.TWO_PHOTON
     # load data
     data, config = load_data(folder_name)
@@ -35,13 +50,3 @@ def main():
     # calculate responsiveness and display it in a nice way
     responsiveness = analysis.responsiveness()
     print(responsiveness)  # TODO: nice appearance
-
-    # Plots
-    plotter = Plotter(analysis)
-
-    plotter.murakami_plot()
-    plotter.anova_window_plot()
-    plotter.polar_grid()
-    plotter.response_map()
-    plotter.sftf_fit()
-    plotter.traceplot()
