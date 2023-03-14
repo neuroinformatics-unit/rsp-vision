ab75f80d16fe7fcd1db40f77186172406cf3c7d6
lauraporta <29216006+lauraporta@users.noreply.github.com>
2023-03-14 13:54:51 +0000

    Add massive refactoring

---
+++ rsp_vision/dashboard/callbacks.py
@@ -0,0 +1,144 @@
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+from dash import Dash, Input, Output, dcc, html
+
+from rsp_vision.analysis.gaussians_calculations import fit_elliptical_gaussian
+from rsp_vision.dashboard.query_dataframes import get_dataframe_for_facet_plot
+
+
+def get_update_fig_all_sessions_callback(app: Dash, signal) -> None:
+    @app.callback(
+        Output("example-graph", "children"), Input("demo-dropdown", "value")
+    )
+    def update_fig_all_sessions(roi_id):
+        lineplot = px.line(
+            signal[signal.roi_id == roi_id],
+            x="frames_id",
+            y="signal",
+            color="session_id",
+        )
+        lineplot.update_traces(line=dict(width=0.3))
+
+        scatterplot = px.scatter(
+            signal[signal.stimulus_onset],
+            x="frames_id",
+            y="sf",
+            color="tf",
+        )
+        fig = go.Figure(data=lineplot.data + scatterplot.data)
+        fig.update_layout(
+            title=f"Signal across sessions, roi: {roi_id + 1}",
+            plot_bgcolor="rgba(0, 0, 0, 0)",
+            paper_bgcolor="rgba(0, 0, 0, 0)",
+            showlegend=False,
+        )
+
+        return html.Div(
+            dcc.Graph(
+                id="my-graph",
+                figure=fig,
+            )
+        )
+
+
+def get_sf_tf_grid_callback(app: Dash, signal, data, counts) -> None:
+    @app.callback(
+        Output("sf_tf-graph", "children"),
+        [
+            Input("demo-dropdown", "value"),
+            Input("directions-checkbox", "value"),
+        ],
+    )
+    def sf_tf_grid(roi_id, dir):
+        vertical_df = get_dataframe_for_facet_plot(
+            signal, data, counts, roi_id, dir
+        )
+
+        fig = px.line(
+            vertical_df,
+            x="stimulus_frames",
+            y="signal",
+            facet_col="sf",
+            facet_row="tf",
+            width=1500,
+            height=800,
+            color="signal_kind",
+        )
+
+        fig.update_layout(
+            title=f"SF TF traces for roi {roi_id + 1}",
+            plot_bgcolor="rgba(0, 0, 0, 0)",
+            paper_bgcolor="rgba(0, 0, 0, 0)",
+        )
+        fig.update_traces(line=dict(width=0.5))
+
+        return html.Div(
+            dcc.Graph(
+                id="sf_tf_plot",
+                figure=fig,
+            )
+        )
+
+
+def get_responses_heatmap_callback(app: Dash, responses, data) -> None:
+    @app.callback(
+        Output("median-response-graph", "children"),
+        [
+            Input("demo-dropdown", "value"),
+            Input("directions-checkbox", "value"),
+        ],
+    )
+    def responses_heatmap(roi_id, dir):
+        median_subtracted_response = (
+            responses[
+                (responses.roi_id == roi_id) & (responses.direction == dir)
+            ]
+            .groupby(["sf", "tf"])[["subtracted"]]
+            .median()
+        )
+        sfs = data._sf[::-1]
+        tfs = data._tf
+
+        array = np.zeros((len(sfs), len(tfs)))
+        for i, sf in enumerate(sfs):
+            for j, tf in enumerate(tfs):
+                array[i, j] = median_subtracted_response.loc[(sf, tf)][
+                    "subtracted"
+                ]
+
+        y_labels = list(map(str, sfs.tolist()))
+        x_labels = list(map(str, tfs.tolist()))
+
+        fig = px.imshow(array, x=x_labels, y=y_labels)
+
+        return html.Div(
+            dcc.Graph(
+                id="median_response_plot",
+                figure=fig,
+            )
+        )
+
+
+def get_gaussian_plot_callback(app: Dash, responses, data) -> None:
+    @app.callback(
+        Output("gaussian-graph", "children"),
+        [
+            Input("demo-dropdown", "value"),
+            Input("directions-checkbox", "value"),
+        ],
+    )
+    def gaussian_plot(roi_id, dir):
+        R = fit_elliptical_gaussian(
+            data._sf[::-1], data._tf, responses, roi_id, data.config, dir
+        )
+        y_labels = list(map(str, data._sf.tolist()))
+        x_labels = list(map(str, data._tf.tolist()))
+        fig = px.imshow(R, x=x_labels, y=y_labels, aspect="equal")
+
+        return html.Div(
+            dcc.Graph(
+                id="gaussian_plot",
+                figure=fig,
+            )
+        )
--- rsp_vision/load/config_switches.py
+++ rsp_vision/load/config_switches.py
@@ -1,10 +1,6 @@
 from ..objects.enums import PhotonType
 
 
-def ascii_array_to_string(array):
-    return "".join([chr(int(i)) for i in array])
-
-
 def get_fps(photon_type, config: dict):
     if photon_type == PhotonType.TWO_PHOTON:
         return config["fps_two_photon"]
