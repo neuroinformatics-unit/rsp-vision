e529b5c1f4ee1a82ffb3b1bf762b4922503c7d4c
lauraporta <ucqflpo@ucl.ac.uk>
2023-04-17 17:55:40 +0100

    Remove changes related to dashboard from this branch

--- demo_dash.py
+++
@@ -1,5 +0,0 @@
-from rsp_vision.dashboard.app import get_app
-
-if __name__ == "__main__":
-    app = get_app()
-    app.run_server(debug=True)
--- rsp_vision/dashboard/styles.py
+++
@@ -1,9 +0,0 @@
-SIDEBAR_STYLE = {
-    "position": "fixed",
-    "top": 0,
-    "left": 0,
-    "bottom": 0,
-    "width": "20rem",
-    "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
-}
