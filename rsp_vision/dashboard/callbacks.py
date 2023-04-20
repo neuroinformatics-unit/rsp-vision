7ab7d18899c06f43c3a17d2710f7ddadba6adf38
lauraporta <ucqflpo@ucl.ac.uk>
2023-04-20 12:47:14 +0100

    Improve line plot of the session

--- rsp_vision/dashboard/callbacks.py
+++ rsp_vision/dashboard/callbacks.py
@@ -17,21 +17,34 @@ def get_update_fig_all_sessions_callback(app: Dash, signal) -> None:
         Output("example-graph", "children"), Input("demo-dropdown", "value")
     )
     def update_fig_all_sessions(roi_id):
-        lineplot = px.line(
-            signal[signal.roi_id == roi_id],
-            x="frames_id",
-            y="signal",
-            color="session_id",
-        )
-        lineplot.update_traces(line=dict(width=0.3))
+        pastel_colors = [
+            "#acd0f8", "#ace7d0", 
+        ]
+
+        unique_session_ids = signal["session_id"].unique()
+        line_data = []
+
+        for i, session_id in enumerate(unique_session_ids):
+            session_data = signal[(signal.roi_id == roi_id) & (signal.session_id == session_id)]
+            line_data.append(
+                go.Scatter(
+                    x=session_data["frames_id"],
+                    y=session_data["signal"],
+                    mode="lines",
+                    line=dict(color=pastel_colors[i % len(pastel_colors)], width=0.3),
+                    showlegend=False,
+                )
+            )
 
         scatterplot = px.scatter(
             signal[signal.stimulus_onset],
             x="frames_id",
             y="sf",
-            color="tf",
         )
-        fig = go.Figure(data=lineplot.data + scatterplot.data)
+
+        scatterplot.update_traces(showlegend=False)
+
+        fig = go.Figure(data=line_data + list(scatterplot.data))
         fig.update_layout(
             title=f"Signal across sessions, roi: {roi_id + 1}",
             plot_bgcolor="rgba(0, 0, 0, 0)",
