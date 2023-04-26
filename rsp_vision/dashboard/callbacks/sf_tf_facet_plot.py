8514e39402a3f15fd39517d963fe29c28dc9885e
lauraporta <ucqflpo@ucl.ac.uk>
2023-04-26 16:47:58 +0100

    Improved facet plot

--- rsp_vision/dashboard/callbacks/sf_tf_facet_plot.py
+++ rsp_vision/dashboard/callbacks/sf_tf_facet_plot.py
@@ -28,8 +28,10 @@ def single_direction_plot(
         y="signal",
         facet_col="tf",
         facet_row="sf",
-        width=1500,
-        height=800,
+        facet_col_spacing=0.005,
+        facet_row_spacing=0.005,
+        width=2000,
+        height=1200,
         color="signal_kind",
         category_orders={
             "sf": data.spatial_frequencies[::-1],
@@ -42,7 +44,17 @@ def single_direction_plot(
         plot_bgcolor="rgba(0, 0, 0, 0)",
         paper_bgcolor="rgba(0, 0, 0, 0)",
     )
-    fig.update_traces(line=dict(width=0.5))
+    for trace in fig.data:
+        if "mean" in trace.name:
+            trace.line.color = "black"
+            trace.line.width = 2
+            trace.line.dash = "solid"
+        elif "median" in trace.name:
+            trace.line.color = "red"
+            trace.line.width = 2
+            trace.line.dash = "solid"
+        else:
+            trace.line.width = 1
 
     return fig
 
@@ -51,6 +63,7 @@ def all_directions_plot(
     signal: np.ndarray,
     data: PhotonData,
     roi_id: int,
+    toggle_value: str,
 ) -> dcc.Graph:
     combined_df = get_dataframe_for_facet_plot_pooled_directions(
         signal, roi_id
@@ -62,8 +75,10 @@ def all_directions_plot(
         y="signal",
         facet_col="tf",
         facet_row="sf",
-        width=1500,
-        height=800,
+        facet_col_spacing=0.005,
+        facet_row_spacing=0.005,
+        width=2000,
+        height=1200,
         color="session_direction",
         category_orders={
             "sf": data.spatial_frequencies[::-1],
@@ -80,7 +95,10 @@ def all_directions_plot(
             trace.line.width = 3
             trace.line.dash = "solid"
         else:
-            trace.line.width = 0.5
+            if "ALL" not in toggle_value:
+                trace.visible = False
+            else:
+                trace.line.width = 0.5
 
     fig.update_layout(
         title=f"SF TF traces for roi {roi_id + 1}",
@@ -120,15 +138,38 @@ def get_sf_tf_grid_callback(
         [
             Input("roi-choice-dropdown", "value"),
             Input("direction-store", "data"),
+            Input("toggle-traces", "value"),
         ],
     )
-    def sf_tf_grid(roi_id: int, direction_input: dict) -> dcc.Graph:
+    def sf_tf_grid(
+        roi_id: int, direction_input: dict, toggle_value: str
+    ) -> dcc.Graph:
         direction = direction_input["value"]
         if direction == "all":
-            fig = all_directions_plot(signal, data, roi_id)
+            fig = all_directions_plot(signal, data, roi_id, toggle_value)
         else:
             fig = single_direction_plot(
                 signal, data, roi_id, direction, counts
+            )
+
+        for i, (x0, x1, text, color) in enumerate(
+            [
+                (0, 75, "gray", "green"),
+                (75, 150, "static", "pink"),
+                (150, 225, "drift", "blue"),
+            ]
+        ):
+            fig.add_vrect(
+                x0=x0,
+                x1=x1,
+                row="all",
+                col="all",
+                annotation_text=text,
+                annotation_position="top left",
+                annotation_font_size=15,
+                fillcolor=color,
+                opacity=0.1,
+                line_width=0,
             )
 
         return html.Div(
