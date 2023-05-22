dcd6e6edf33f7a10c287bb122994f7acf13a301e
lauraporta <ucqflpo@ucl.ac.uk>
2023-05-22 15:35:22 +0100

    Start to load data in dashboard

--- rsp_vision/dashboard_2/app.py
+++ rsp_vision/dashboard_2/app.py
@@ -2,42 +2,42 @@ import dash
 import dash_mantine_components as dmc
 from dash import Dash, dcc, html
 
-link = "https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
-external_stylesheets = [
-    {
-        "href": link,
-        "rel": "stylesheet",
-    }
-]
-# fmt: off
-app = Dash(
-    __name__,
-    use_pages=True,
-    external_stylesheets=external_stylesheets,
-    suppress_callback_exceptions=True,
-)
-# fmt: on
 
-header = dmc.Header(
-    height=70,
-    children=[
-        dmc.Title(
-            "RSP Vision",
-            order=1,
-            className="main-title",
-        )
-    ],
-    className="header",
-)
-
-app.layout = html.Div(
-    [
-        dcc.Store(id="store", data={}),
-        header,
-        dash.page_container,
+def get_app() -> Dash:
+    link = "https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+    external_stylesheets = [
+        {
+            "href": link,
+            "rel": "stylesheet",
+        }
     ]
-)
+    # fmt: off
+    app = Dash(
+        __name__,
+        use_pages=True,
+        external_stylesheets=external_stylesheets,
+        suppress_callback_exceptions=True,
+    )
+    # fmt: on
 
+    header = dmc.Header(
+        height=70,
+        children=[
+            dmc.Title(
+                "RSP Vision",
+                order=1,
+                className="main-title",
+            )
+        ],
+        className="header",
+    )
 
-if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.layout = html.Div(
+        [
+            dcc.Store(id="store", data={}),
+            header,
+            dash.page_container,
+        ]
+    )
+
+    return app
