a391fa10bcacc32a39c6764065974c0e361b6de4
lauraporta <ucqflpo@ucl.ac.uk>
2023-05-22 15:02:18 +0100

    Working data storage

--- rsp_vision/dashboard_2/pages/data_table.py
+++ rsp_vision/dashboard_2/pages/data_table.py
@@ -1,6 +1,7 @@
 import dash
+import dash_bootstrap_components as dbc
 import dash_mantine_components as dmc
-from dash import Input, Output, callback, dash_table, dcc
+from dash import Input, Output, callback, dash_table
 
 dash.register_page(__name__, path="/")
 
@@ -30,7 +31,7 @@ data = [
 ]
 
 layout = dash.html.Div(
-    [   
+    [
         dmc.Title(
             "Data Table",
             order=2,
@@ -42,10 +43,11 @@ layout = dash.html.Div(
                     id="selected_data_str",
                     className="selected-data-text",
                 ),
-                dmc.Button(
+                dbc.Button(
                     "Load Data",
                     id="button",
                     className="load-data-button",
+                    href="/murakami_plot",
                     n_clicks=0,
                 ),
             ],
@@ -72,32 +74,55 @@ layout = dash.html.Div(
             ],
             className="table",
         ),
-        dcc.Store(id="store"),
-        dcc.Location(id="redirect"),
+        dmc.Text(
+            id="stored_data1",
+            className="selected-data-text",
+        ),
+        # dcc.Location(
+        #     id="redirect",
+        #     refresh=True,
+        #     ),
     ],
     className="page",
 )
 
 
 @callback(
-        [Output("selected_data_str", "children"), 
-         Output("store", "data"),
-         Output("button", "disabled")],
-        Input("table", "selected_rows")
-    )
+    [
+        Output("selected_data_str", "children"),
+        Output("store", "data"),
+        Output("button", "disabled"),
+    ],
+    Input("table", "selected_rows"),
+)
 def update_graphs(selected_rows):
     if selected_rows is None or len(selected_rows) == 0:
-        return "Select data to be loaded", [], True
+        return "Select data to be loaded", {}, True
     else:
-        return f'Selected data: {data[selected_rows[0]]["Element Name"]}', data[selected_rows[0]], False
+        store = {"data": "test content", "other": "other test"}
+        return (
+            f'Selected data: {data[selected_rows[0]]["Element Name"]}',
+            store,  # data[selected_rows[0]]},
+            False,
+        )
+
+
+# @callback(
+#         Output("redirect", "pathname"),
+#         Input("button", "n_clicks")
+#     )
+# def redirect(n_clicks):
+#     if n_clicks is None or n_clicks == 0:
+#         pass
+#     else:
+#         return "/murakami_plot"
+
 
 @callback(
-        Output("redirect", "pathname"),
-        Input("button", "n_clicks")
-    )
-def redirect(n_clicks):
-    if n_clicks is None or n_clicks == 0:
-        pass
-    else:
-        return "/murakami_plot"
-        
+    Output("stored_data1", "children"),
+    Input("store", "data"),
+)
+def read_stored_data(store):
+    if store == {}:
+        return "Store is None"
+    return str(store["data"])
