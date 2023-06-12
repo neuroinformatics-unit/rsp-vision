a79e0909d3d706a217151bd35421b2fba49757c8
lauraporta <ucqflpo@ucl.ac.uk>
2023-06-12 14:34:10 +0100

    Add more descriptions / docs

--- tests/fixtures_helpers.py
+++ tests/fixtures_helpers.py
@@ -12,9 +12,27 @@ from rsp_vision.objects.photon_data import PhotonData
 
 
 def get_shared_variables_to_generate_mock_data():
-    """_summary_
+    """
+    This function returns the variables that are used to generate the mock
+    data, and are designed to replicate the structure of the experiment.
 
+    =======================================
+    Description of the stimuli parameters
+    =======================================
+    In this simulated data we have blocks of 2 sessions (which can be repeated
+    in multiple days), 2 spatial frequencies, 2 temporal frequencies and 2
+    directions, for a total of 8 unique stimuli. Each session has 3 repetitions
+    of each unique stimulus, for a total of 24 stimuli per session.
+
+    As in the real data, the number of baseline triggers is 4, and the number
+    of frames per trigger is 75.
+
+    With this information, I made up a matrix of stimuli that is used to
+    generate the mock data. The matrix is as follows:
+
+    =======================================
     Matrix of sf / tf / direction stims
+    =======================================
     Day 1 and day 2 are identical
     Session 1 (combinations are repeated 3 times)
     sf:   1 1 1  1 1 1  1 1 1  1 1 1
@@ -26,14 +44,27 @@ def get_shared_variables_to_generate_mock_data():
     tf:   3 3 4  4 3 3  4 4 3  3 4 4
     dir:  6 5 6  5 6 5  6 5 6  5 6 5
 
-    All these variables are used locally to generate the mock data
-    and are retreived from there by the tests
-    It is useful to have them here more ecplicit for debugging purposes
-
     Returns
     -------
-    _type_
-        _description_
+    tuple(namedtuple, namedtuple, namedtuple)
+        Nametuple used:
+        - `Parameters`:
+            - n_roi: number of rois
+            - n_baseline_triggers: number of baseline triggers
+            - n_triggers_per_stim: number of triggers per stimulus
+            - n_frames_per_trigger: number of frames per trigger
+            - n_unique_stim: number of unique stimuli
+            - n_repetition: number of repetitions per unique stimulus
+        - `SessionParameters`:
+            - `sf`: spatial frequencies
+            - `tf`: temporal frequencies
+            - `dir`: directions
+        Mores specifically the output is: (session_1, session_2, parameters)
+
+        These named tuples contain parameters that are used by
+        `make_stim_dict` to create the `stimulus` dict, which is a
+        fundamental part of the mock data. The first two namedtuples contain
+        the information regarding two unique sessions.
     """
 
     parameters = namedtuple(
@@ -94,7 +125,16 @@ def get_shared_variables_to_generate_mock_data():
 
 def make_variables_day_related(p, multiple_days=False):
     """
+    This method returns other variables that are used to generate the mock
+    data, that are influenced by the number of days of the experiment.
+    The content of the session is not influenced by the number of days, but
+    the number of sessions is, together with the number of stimuli.
+
+    In order to better understand them, here a schematic of a session:
+
+    =======================================
     Structure of a session
+    =======================================
     --- Session 1 -----------------------------------
     === n_baseline_triggers =========================
     === a subset of n_unique_stim ===================
@@ -105,15 +145,22 @@ def make_variables_day_related(p, multiple_days=False):
 
     Parameters
     ----------
-    p : _type_
-        _description_
+    p : namedtuple
+        This is the namedtuple that contains the parameters of the experiment,
+        as returned by `get_shared_variables_to_generate_mock_data`.
     multiple_days : bool, optional
-        _description_, by default False
+        Whether the experiment has multiple days or not, by default False.
+        If True, the `n_days` variable is 2, otherwise it is 1.
 
     Returns
     -------
-    _type_
-        _description_
+    namedtuple
+        This namedtuple contains the following variables:
+        - n_sessions: number of sessions
+        - len_session: length of a session
+        - n_stim: number of stimuli
+        - n_days: number of days
+        - day_stimulus: stimulus of each day
     """
 
     if multiple_days:
@@ -156,6 +203,15 @@ def make_variables_day_related(p, multiple_days=False):
 
 
 def get_config_mock():
+    """
+    This method returns the config file used to generate the mock data.
+    It is a subset of the config file used in the real experiment.
+
+    Returns
+    -------
+    dict
+        Dictionary containing the config file.
+    """
     return {
         "fps_two_photon": 30,
         "trigger_interval_s": 2.5,
@@ -187,6 +243,32 @@ def make_stim_dict(
     cycles_per_visual_degree,
     cycles_per_second,
 ):
+    """
+    This method returns the stimulus dictionary used to generate the mock data.
+    It is based on the variables set by the methods
+    `get_shared_variables_to_generate_mock_data` and
+    `make_variables_day_related`.
+
+    Parameters
+    ----------
+    n_baseline_triggers : int
+        Number of baseline triggers.
+    n_stim_per_session : int
+        Number of stimuli per session.
+    n_triggers_per_stim : int
+        Number of triggers per stimulus.
+    directions : list
+        List of directions.
+    cycles_per_visual_degree : list
+        List of cycles per visual degree. Refers to the spatial frequency.
+    cycles_per_second : list
+        List of cycles per second. Refers to the temporal frequency.
+
+    Returns
+    -------
+    dict
+        Dictionary containing the stimulus information.
+    """
     return {
         "screen_size": "screen_size",
         "stimulus": {
@@ -204,6 +286,32 @@ def make_stim_dict(
 
 
 def make_random_responses(seed_number, n_sessions, n_roi, len_session):
+    """
+    This method generates random responses for the mock data starting from
+    a seed number. The random numbers are drawn from a Poisson distribution
+    with a random expected value. The sign of the responses is flipped
+    randomly.
+
+    Parameters
+    ----------
+    seed_number : int
+        Seed number.
+    n_sessions : int
+        Number of sessions.
+    n_roi : int
+        Number of ROIs.
+    len_session : int
+        Length of a session.
+
+    Returns
+    -------
+    np.ndarray
+        Array containing the random responses. The shape is
+        (n_sessions, n_roi, len_session). The information regarding the
+        number of days is contained in the number of sessions. For different
+        days different random responses are generated.
+    """
+
     np.random.seed(seed_number)
     expected_value = np.abs(np.random.randint(50))  # use positive values only
     data = np.random.poisson(
@@ -220,6 +328,35 @@ def make_raw_data_dict_mock(
     session2,
     params,
 ):
+    """
+    This method assembles together the dicts and variables to generate the
+    mock data that are produced thanks to the other methods in this module.
+    It replicates the structure that `DataRaw` class is expected to receive.
+    This allows us to have a mock data that is digestible by the same pipeline
+    used for the real data.
+
+    Parameters
+    ----------
+    seed_number : int
+        Seed number used to generate the random responses.
+    day_vars : namedtuple
+        Namedtuple containing the variables related to the day, generated by
+        the method `make_variables_day_related`.
+    session1 : namedtuple
+        Namedtuple containing the variables related to the first session,
+        generated by the method `get_shared_variables_to_generate_mock_data`.
+    session2 : namedtuple
+        Namedtuple containing the variables related to the second session,
+        generated by the method `get_shared_variables_to_generate_mock_data`.
+    params : namedtuple
+        Namedtuple containing the parameters used to generate the mock data,
+        generated by the method `get_shared_variables_to_generate_mock_data`.
+
+    Returns
+    -------
+    dict
+        Dictionary containing the mock data.
+    """
     return {
         "day": {
             "roi": "roi",
@@ -256,6 +393,21 @@ def make_raw_data_dict_mock(
 
 
 def get_data_raw_object_mock(seed_number=1, multiple_days=False):
+    """
+    This method generates a mock `DataRaw` object with simulated data.
+
+    Parameters
+    ----------
+    seed_number : int, optional
+        The seed number used to generate the random responses, by default 1
+    multiple_days : bool, optional
+        Weather to generate data for multiple days or not, by default False
+
+    Returns
+    -------
+    DataRaw
+        The mock `DataRaw` object.
+    """
     session1, session2, params = get_shared_variables_to_generate_mock_data(
         multiple_days
     )
@@ -274,6 +426,21 @@ def get_data_raw_object_mock(seed_number=1, multiple_days=False):
 
 
 def get_photon_data_mock(seed_number=1, multiple_days=False):
+    """
+    This method generates a mock `PhotonData` object with simulated data.
+
+    Parameters
+    ----------
+    seed_number : int, optional
+        The seed number used to generate the random responses, by default 1
+    multiple_days : bool, optional
+        Weather to generate data for multiple days or not, by default False
+
+    Returns
+    -------
+    PhotonData
+        The mock `PhotonData` object.
+    """
     photon_data = PhotonData.__new__(PhotonData)
     photon_data.using_real_data = False
     photon_data.photon_type = PhotonType.TWO_PHOTON
@@ -288,6 +455,22 @@ def get_photon_data_mock(seed_number=1, multiple_days=False):
 
 
 def get_response_mock(seed_number, multiple_days=False):
+    """
+    This method generates a mock `FrequencyResponsiveness` object with
+    simulated data.
+
+    Parameters
+    ----------
+    seed_number : int, optional
+        The seed number used to generate the random responses, by default 1
+    multiple_days : bool, optional
+        Weather to generate data for multiple days or not, by default False
+
+    Returns
+    -------
+    FrequencyResponsiveness
+        The mock `FrequencyResponsiveness` object.
+    """
     pt = PhotonType.TWO_PHOTON
     return FrequencyResponsiveness(
         PhotonData(
