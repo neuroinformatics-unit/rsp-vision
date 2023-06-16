a064a7e800950ad448ca08a42438fd330ca5d06a
lauraporta <ucqflpo@ucl.ac.uk>
2023-06-16 14:19:34 +0100

    Change app.py in order to be called by tests

--- demo_dash.py
+++ demo_dash.py
@@ -1,5 +1,5 @@
-from rsp_vision.dashboard.app import get_app
+from rsp_vision.dashboard.app import app
 
 if __name__ == "__main__":
-    app = get_app()
+    # app = get_app()
     app.run_server(debug=True)
