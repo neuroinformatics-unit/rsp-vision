e3b708ed635de3665af2ca9f7e05f3ee0d5b7d69
lauraporta <ucqflpo@ucl.ac.uk>
2023-06-16 14:19:34 +0100

    Change app.py in order to be called by tests

--- rsp_vision/dashboard/app.py
+++ rsp_vision/dashboard/app.py
@@ -2,42 +2,38 @@ import dash
 import dash_mantine_components as dmc
 from dash import Dash, dcc, html
 
+link = "https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+external_stylesheets = [
+    {
+        "href": link,
+        "rel": "stylesheet",
+    }
+]
+# fmt: off
+app = Dash(
+    __name__,
+    use_pages=True,
+    external_stylesheets=external_stylesheets,
+    suppress_callback_exceptions=True,
+)
+# fmt: on
 
-def get_app() -> Dash:
-    link = "https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
-    external_stylesheets = [
-        {
-            "href": link,
-            "rel": "stylesheet",
-        }
+header = dmc.Header(
+    height=70,
+    children=[
+        dmc.Title(
+            "RSP vision 👁️",
+            order=1,
+            className="main-title",
+        )
+    ],
+    className="header",
+)
+
+app.layout = html.Div(
+    [
+        dcc.Store(id="store", data={}),
+        header,
+        dash.page_container,
     ]
-    # fmt: off
-    app = Dash(
-        __name__,
-        use_pages=True,
-        external_stylesheets=external_stylesheets,
-        suppress_callback_exceptions=True,
-    )
-    # fmt: on
-
-    header = dmc.Header(
-        height=70,
-        children=[
-            dmc.Title(
-                "RSP vision 👁️",
-                order=1,
-                className="main-title",
-            )
-        ],
-        className="header",
-    )
-
-    app.layout = html.Div(
-        [
-            dcc.Store(id="store", data={}),
-            header,
-            dash.page_container,
-        ]
-    )
-
-    return app
+)
