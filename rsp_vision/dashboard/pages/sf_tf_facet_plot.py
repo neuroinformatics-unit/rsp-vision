e02719fe13813b29a1af9d89c72e55eabdf29056
lauraporta <ucqflpo@ucl.ac.uk>
2023-06-20 17:03:40 +0100

    Add functioning scaffold - plots to be fixed

--- rsp_vision/dashboard/pages/sf_tf_facet_plot.py
+++ rsp_vision/dashboard/pages/sf_tf_facet_plot.py
@@ -1,14 +1,22 @@
-from collections import namedtuple
 import itertools
-from pathlib import Path
 import pickle
+from collections import namedtuple
+from pathlib import Path
+
 import dash
 import dash_mantine_components as dmc
-from dash import html, dcc, callback, Input, Output
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+import plotly.subplots as sp
+from dash import Input, Output, callback, dcc, html
+from scipy.stats import pearsonr
 
+# from rsp_vision.dashboard.pages.murakami_plot import load_data
+from rsp_vision.analysis.gaussians_calculations import (
+    get_gaussian_matrix_to_be_plotted,
+)
 from rsp_vision.objects.photon_data import PhotonData
 
 dash.register_page(__name__, path="/sf_tf_facet_plot")
@@ -70,7 +78,7 @@ layout = html.Div(
                 dmc.Col(
                     [
                         html.Div(
-                            id="gaussian-plot",
+                            id="gaussian-graph-andermann",
                             className="gaussian-plot",
                         ),
                     ],
@@ -89,12 +97,26 @@ def load_data(store):
         Path(store["path"])
         / store["subject_folder_path"]
         / store["session_folder_path"]
+        / "gaussians_fits_and_roi_info.pickle"
     )
-    
+    with open(path, "rb") as f:
+        data = pickle.load(f)
+
+    return data
+
+
+def load_data_of_signal_dataframe(store):
+    path = (
+        Path(store["path"])
+        / store["subject_folder_path"]
+        / store["session_folder_path"]
+    )
+
     with open(path / "roi_0_signal_dataframe.pickle", "rb") as f:
         signal = pickle.load(f)
 
     return signal
+
 
 @callback(
     Output("sf-tf-plot", "children"),
@@ -105,13 +127,14 @@ def load_data(store):
     #     # Input("toggle-traces", "value"),
     # ],
 )
-def sf_tf_grid(store
+def sf_tf_grid(
+    store,
     # roi_id: int, direction_input: dict, toggle_value: str
 ) -> dcc.Graph:
     if store == {}:
         return "No data to plot"
 
-    signal = load_data(store)
+    signal = load_data_of_signal_dataframe(store)
     spatial_frequencies = store["config"]["spatial_frequencies"]
     temporal_frequencies = store["config"]["temporal_frequencies"]
 
@@ -224,11 +247,11 @@ def sf_tf_grid(store
     )
 
 
-def get_df_sf_tf_combo_plot(
-    signal: pd.DataFrame
-) -> np.ndarray:
+def get_df_sf_tf_combo_plot(signal: pd.DataFrame) -> np.ndarray:
     signal["stimulus_frames"] = np.nan
-    n_frames_per_stim = int( 75 * 3
+    n_frames_per_stim = int(
+        75
+        * 3
         # data.n_frames_per_trigger * data.n_triggers_per_stimulus
     )
     counts = np.arange(0, n_frames_per_stim)
@@ -245,7 +268,7 @@ def get_df_sf_tf_combo_plot(
         signal.loc[start:end, "session_id"] = signal.loc[idx, "session_id"]
 
     return counts
-    
+
 
 def get_dataframe_for_facet_plot_pooled_directions(
     signal: pd.DataFrame,
@@ -373,3 +396,304 @@ def get_dataframe_for_facet_plot(
     )
 
     return vertical_df
+
+
+@callback(
+    Output("gaussian-graph-andermann", "children"),
+    Input("store", "data"),
+    # [
+    #     Input("roi-choice-dropdown", "value"),
+    #     Input("direction-store", "data"),
+    # ],
+)
+def gaussian_plot(
+    store: dict,
+    # roi_id: int, direction_input: dict
+) -> html.Div:
+    if store == {}:
+        return "No data to plot"
+
+    data = load_data(store)
+
+    median_subtracted_responses = data["median_subtracted_responses"]
+    fit_outputs = data["fit_outputs"]
+
+    if store == {}:
+        return "No data to plot"
+
+    signal = load_data_of_signal_dataframe(store)
+    spatial_frequencies = store["config"]["spatial_frequencies"]
+    temporal_frequencies = store["config"]["temporal_frequencies"]
+
+    # direction = direction_input["value"]
+    direction = 90
+    roi_id = 0
+    get_df_sf_tf_combo_plot(signal)
+
+    #  from data I need: n_days, sf_tf_combinations
+    sf_tf_combinations = itertools.product(
+        spatial_frequencies, temporal_frequencies
+    )
+    total_n_days = signal.day.max()
+
+    Data = namedtuple("data", ["sf_tf_combinations", "total_n_days"])
+    data = Data(sf_tf_combinations, total_n_days)
+
+    # direction = direction_input["value"]
+    low_res_gaussian = get_6x6_gaussian(
+        roi_id,
+        fit_outputs,
+        spatial_frequencies,
+        temporal_frequencies,
+        direction,
+    )
+    high_res_gaussian = get_custom_gaussian(
+        roi_id,
+        fit_outputs,
+        spatial_frequencies,
+        temporal_frequencies,
+        direction,
+    )
+
+    # Create subplots for the two Gaussian plots
+    fig = sp.make_subplots(
+        rows=3,
+        cols=1,
+        subplot_titles=(
+            "Median subtracted response",
+            "Original Gaussian",
+            "Oversampled Gaussian",
+        ),
+    )
+    uniform_sfs = uniform_tfs = np.arange(0, len(spatial_frequencies), 1)
+
+    if isinstance(direction, int) and direction != "all":
+        #  Add the heatmap for the median subtracted response
+        fig.add_trace(
+            go.Heatmap(
+                z=median_subtracted_responses[(roi_id, direction)],
+                x=uniform_tfs,
+                y=uniform_sfs,
+                colorscale="Viridis",
+                showscale=False,
+            ),
+            row=1,
+            col=1,
+        )
+
+        # Add the heatmap for the original Gaussian
+        fig.add_trace(
+            go.Heatmap(
+                # z=downsampled_gaussians[(roi_id, direction)],
+                z=low_res_gaussian,
+                x=uniform_tfs,
+                y=uniform_sfs,
+                colorscale="Viridis",
+                showscale=False,
+            ),
+            row=2,
+            col=1,
+        )
+
+        # Add the heatmap for the oversampled Gaussian
+        # I tried with the log plot, it does not look good
+        oversampling_factor = 100
+        uniform_oversampled_sfs = np.linspace(
+            0, oversampling_factor - 1, oversampling_factor
+        )
+        uniform_oversampled_tfs = np.linspace(
+            0, oversampling_factor - 1, oversampling_factor
+        )
+
+        fig.add_trace(
+            go.Heatmap(
+                # z=oversampled_gaussians[(roi_id, direction)],
+                z=high_res_gaussian,
+                x=uniform_oversampled_tfs,
+                y=uniform_oversampled_sfs,
+                colorscale="Viridis",
+                showscale=False,
+            ),
+            row=3,
+            col=1,
+        )
+
+        log_sfs = np.linspace(
+            min(spatial_frequencies),
+            max(spatial_frequencies),
+            num=oversampling_factor,
+        )
+
+        log_tfs = np.linspace(
+            min(temporal_frequencies),
+            max(temporal_frequencies),
+            num=oversampling_factor,
+        )
+
+        fit_corr = fit_correlation(
+            # downsampled_gaussians[(roi_id, direction)],
+            low_res_gaussian,
+            median_subtracted_responses[(roi_id, direction)],
+        )
+    else:
+        #  Add the heatmap for the median subtracted response
+        fig.add_trace(
+            go.Heatmap(
+                z=median_subtracted_responses[(roi_id, "pooled")],
+                x=uniform_tfs,
+                y=uniform_sfs,
+                colorscale="Viridis",
+                showscale=False,
+            ),
+            row=1,
+            col=1,
+        )
+
+        # Add the heatmap for the original Gaussian
+        fig.add_trace(
+            go.Heatmap(
+                # z=downsampled_gaussians[(roi_id, "pooled")],
+                z=low_res_gaussian,
+                x=uniform_tfs,
+                y=uniform_sfs,
+                colorscale="Viridis",
+                showscale=False,
+            ),
+            row=2,
+            col=1,
+        )
+
+        # Add the heatmap for the oversampled Gaussian
+        oversampling_factor = 100
+        uniform_oversampled_sfs = np.linspace(
+            0, oversampling_factor - 1, oversampling_factor
+        )
+        uniform_oversampled_tfs = np.linspace(
+            0, oversampling_factor - 1, oversampling_factor
+        )
+
+        fig.add_trace(
+            go.Heatmap(
+                # z=oversampled_gaussians[(roi_id, "pooled")],
+                z=high_res_gaussian,
+                x=uniform_oversampled_tfs,
+                y=uniform_oversampled_sfs,
+                colorscale="Viridis",
+                showscale=False,
+            ),
+            row=3,
+            col=1,
+        )
+
+        log_sfs = np.linspace(
+            min(spatial_frequencies),
+            max(spatial_frequencies),
+            num=oversampling_factor,
+        )
+
+        log_tfs = np.linspace(
+            min(temporal_frequencies),
+            max(temporal_frequencies),
+            num=oversampling_factor,
+        )
+
+        fit_corr = fit_correlation(
+            # downsampled_gaussians[(roi_id, "pooled")],
+            low_res_gaussian,
+            median_subtracted_responses[(roi_id, "pooled")],
+        )
+
+    fit_value = (
+        fit_outputs[(roi_id, direction)][-1]
+        if isinstance(direction, int) and direction != "all"
+        else fit_outputs[(roi_id, "pooled")][-1]
+    )
+
+    # Update layout to maintain the aspect ratio
+    fig.update_layout(
+        autosize=False,
+        width=400,
+        height=1000,
+        margin=dict(t=50, b=50, l=50, r=50),
+        showlegend=False,
+        title_text=f"Fit Correlation: {fit_corr:.2f}, 𝜁: {fit_value:.2f}",
+    )
+
+    fig.update_xaxes(
+        tickvals=uniform_tfs, ticktext=temporal_frequencies, row=1, col=1
+    )
+    fig.update_yaxes(
+        tickvals=uniform_sfs, ticktext=spatial_frequencies, row=1, col=1
+    )
+    fig.update_xaxes(
+        tickvals=uniform_tfs, ticktext=temporal_frequencies, row=2, col=1
+    )
+    fig.update_yaxes(
+        tickvals=uniform_sfs, ticktext=spatial_frequencies, row=2, col=1
+    )
+    fig.update_yaxes(
+        tickvals=uniform_oversampled_sfs[::10],
+        ticktext=np.round(log_sfs[::10], 2),
+        row=3,
+        col=1,
+    )
+    fig.update_xaxes(
+        tickvals=uniform_oversampled_tfs[::10],
+        ticktext=np.round(log_tfs[::10], 2),
+        row=3,
+        col=1,
+    )
+
+    fig.update_yaxes(title_text="Spatial Frequency", row=1, col=1)
+    fig.update_xaxes(title_text="Temporal Frequency", row=1, col=1)
+    fig.update_yaxes(title_text="Spatial Frequency", row=2, col=1)
+    fig.update_xaxes(title_text="Temporal Frequency", row=2, col=1)
+    fig.update_yaxes(title_text="Spatial Frequency", row=3, col=1)
+    fig.update_xaxes(title_text="Temporal Frequency", row=3, col=1)
+
+    return html.Div(
+        dcc.Graph(
+            id="gaussian_plot",
+            figure=fig,
+        )
+    )
+
+
+def fit_correlation(
+    gaussian: np.ndarray, median_subtracted_response: np.ndarray
+) -> float:
+    fit_corr, _ = pearsonr(
+        median_subtracted_response.flatten(), gaussian.flatten()
+    )
+    return fit_corr
+
+
+def get_6x6_gaussian(
+    roi_id, fit_outputs, spatial_frequencies, temporal_frequencies, direction
+):
+    matrix = get_gaussian_matrix_to_be_plotted(
+        kind="6x6 matrix",
+        roi_id=roi_id,
+        fit_output=fit_outputs,
+        sfs=np.asarray(spatial_frequencies),
+        tfs=np.asarray(temporal_frequencies),
+        pooled_directions=False,
+        direction=direction,
+    )
+    return matrix
+
+
+def get_custom_gaussian(
+    roi_id, fit_outputs, spatial_frequencies, temporal_frequencies, direction
+):
+    matrix = get_gaussian_matrix_to_be_plotted(
+        kind="custom",
+        roi_id=roi_id,
+        fit_output=fit_outputs,
+        sfs=np.asarray(spatial_frequencies),
+        tfs=np.asarray(temporal_frequencies),
+        pooled_directions=False,
+        direction=direction,
+        matrix_definition=100,
+    )
+    return matrix
