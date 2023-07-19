72d3af221d085932b40325a4dbead7bfc3c7c913
Laura Porta <ucqflpo@ucl.ac.uk>
2023-07-19 16:45:48 +0100

    Dashboard version 1.0, datatable and murakami plot (#61)
    Co-authored-by: Niko Sirmpilatze <niko.sirbiladze@gmail.com>
    Co-authored-by: Joe Ziminski <55797454+JoeZiminski@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ rsp_vision/dashboard/app.py
@@ -0,0 +1,48 @@
+# This file is used to create the Dash app and the scaffold layout.
+import dash_mantine_components as dmc
+from dash import Dash, dcc, html, page_container
+
+# I am using Open Sans font from Google Fonts for the whole app.
+# The design of the app is controlled by the CSS file in
+# rsp_vision/dashboard/assets/style.css.
+google_fonts_link = (
+    "https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+)
+external_stylesheets = [
+    {
+        "href": google_fonts_link,
+        "rel": "stylesheet",
+    }
+]
+# fmt: off
+app = Dash(
+    __name__,
+    use_pages=True,
+    external_stylesheets=external_stylesheets,
+    suppress_callback_exceptions=True,
+)
+# fmt: on
+
+header = dmc.Header(
+    height=70,
+    children=[
+        dmc.Title(
+            "RSP vision 👁️",
+            order=1,
+            className="main-title",
+        )
+    ],
+    className="header",
+)
+
+# Here I define the layout of the app. The layout is a composition of
+# Mantine components and Dash components.
+# Importantly, here I define the `Store` component, which is fundamental
+# to share information between pages.
+app.layout = html.Div(
+    [
+        dcc.Store(id="store", data={}),
+        header,
+        page_container,
+    ]
+)
