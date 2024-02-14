6c314d147f58ffb85a9ecde57efcfd2711ea3de6
lauraporta <ucqflpo@ucl.ac.uk>
2024-02-14 13:28:14 +0000

    Analyse overall activity in response and baseline, make figures

--- scripts/penk/spontaneous_activity_plots.py
+++ scripts/penk/spontaneous_activity_plots.py
@@ -2,21 +2,162 @@ import pickle
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 import seaborn as sns
 
-remote_path = Path("/Volumes/margrie-1/laura/")
-local_plots_path = Path(
-    "/Users/lauraporta/local_data/rsp_vision/derivatives/figures/"
+local_path = Path(
+    "/Users/lauraporta/local_data/rsp_vision/derivatives/penk_analysis/"
 )
-with open(remote_path / "activity.pickle", "rb") as f:
+local_plots_path = Path(
+    "/Users/lauraporta/local_data/rsp_vision/derivatives/penk_analysis/figures/"
+)
+
+with open(local_path / "activity.pickle", "rb") as f:
     activity = pickle.load(f)
 
 
 #  heatmap for each dataset
 
 for datagroup in activity.keys():
-    for dataset in datagroup.keys():
-        sns.heatmap(activity[datagroup][dataset]["baseline"])
-        sns.heatmap(activity[datagroup][dataset]["response"])
-        plt.savefig(local_plots_path / f"{dataset}_heatmap.png")
+    for dataset in activity[datagroup].keys():
+        min = 0
+        max = 900
+        sns.heatmap(
+            activity[datagroup][dataset]["baseline"], vmin=min, vmax=max
+        )
+        plt.savefig(
+            local_plots_path / f"{dataset}_baseline_heatmap.png", dpi=200
+        )
         plt.close()
+        sns.heatmap(
+            activity[datagroup][dataset]["response"], vmin=min, vmax=max
+        )
+        plt.savefig(
+            local_plots_path / f"{dataset}_response_heatmap.png", dpi=200
+        )
+        plt.close()
+
+
+#  mean activity for each neuron across all the seesion:
+fig, ax = plt.subplots(5, 2, figsize=(15, 15))
+
+
+for j, datagroup in enumerate(activity.keys()):
+    for k, dataset in enumerate(activity[datagroup].keys()):
+        mean_baseline = []
+        mean_response = []
+        for i in range(len(activity[datagroup][dataset]["baseline"])):
+            mean_baseline.append(
+                np.nanmean(activity[datagroup][dataset]["baseline"][i])
+            )
+            mean_response.append(
+                np.nanmean(activity[datagroup][dataset]["response"][i])
+            )
+
+        ax[k, j].scatter(
+            np.arange(len(mean_baseline)), mean_baseline, label="baseline"
+        )
+        ax[k, j].scatter(
+            np.arange(len(mean_response)), mean_response, label="response"
+        )
+        ax[k, j].set_title(dataset)
+        ax[k, j].legend()
+        ax[k, j].set_xlabel("Neuron")
+        ax[k, j].set_ylabel("Mean activity")
+
+        #  column title according to datagroup
+        # ax[0, j].set_title(datagroup)
+plt.tight_layout()
+
+# set all y lim
+for i in range(5):
+    for j in range(2):
+        ax[i, j].set_ylim(0, 50)
+
+#  reduce wspace
+plt.subplots_adjust(wspace=0.1)
+plt.savefig(local_plots_path / "mean_activity.png", dpi=200)
+plt.close()
+
+
+# boxplots
+fig2, ax2 = plt.subplots(1, 2, figsize=(10, 10))
+
+for j, datagroup in enumerate(activity.keys()):
+    df = pd.DataFrame()
+    baseline = []
+    response = []
+    for k, dataset in enumerate(activity[datagroup].keys()):
+        mean_baseline = []
+        mean_response = []
+        for i in range(len(activity[datagroup][dataset]["baseline"])):
+            mean_baseline.append(
+                np.nanmean(activity[datagroup][dataset]["baseline"][i])
+            )
+            mean_response.append(
+                np.nanmean(activity[datagroup][dataset]["response"][i])
+            )
+        baseline += mean_baseline
+        response += mean_response
+
+    df["baseline"] = baseline
+    df["response"] = response
+    sns.stripplot(data=df, color="0.25", ax=ax2[j], marker=".", alpha=0.5)
+    sns.boxplot(data=df, ax=ax2[j], showfliers=False)
+    #  place on top a swarmplot
+
+    ax2[j].set_title(datagroup)
+    ax2[j].set_xlabel("Static (B) vs Gray (R)")
+    ax2[j].set_ylabel("Mean activity")
+    ax2[j].set_xticks([])
+
+# set all y lim
+for j in range(2):
+    ax2[j].set_ylim(-10, 50)
+plt.tight_layout()
+#  no upper and right axis
+sns.despine()
+plt.savefig(local_plots_path / "boxplots.png", dpi=200)
+plt.close()
+
+
+#  scatterplot baseline vs response
+
+fig3, ax3 = plt.subplots(1, 2, figsize=(10, 5))
+
+
+for j, datagroup in enumerate(activity.keys()):
+    df = pd.DataFrame()
+    baseline = []
+    response = []
+    for k, dataset in enumerate(activity[datagroup].keys()):
+        mean_baseline = []
+        mean_response = []
+        for i in range(len(activity[datagroup][dataset]["baseline"])):
+            mean_baseline.append(
+                np.nanmean(activity[datagroup][dataset]["baseline"][i])
+            )
+            mean_response.append(
+                np.nanmean(activity[datagroup][dataset]["response"][i])
+            )
+        baseline += mean_baseline
+        response += mean_response
+
+    df["baseline"] = baseline
+    df["response"] = response
+
+    sns.scatterplot(
+        data=df, x="baseline", y="response", ax=ax3[j], alpha=0.5, markers="."
+    )
+    ax3[j].set_title(datagroup)
+    ax3[j].set_xlabel("Mean baseline activity")
+    ax3[j].set_ylabel("Mean response activity")
+    ax3[j].set_xlim(-10, 50)
+    ax3[j].set_ylim(-10, 50)
+
+plt.tight_layout()
+#  no upper and right axis
+sns.despine()
+plt.savefig(local_plots_path / "scatterplot.png", dpi=200)
+plt.close()
